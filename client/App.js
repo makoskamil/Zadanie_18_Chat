c131b0850fae81e6b410d1861823fcca9f962440
Kamil Makoś <makos.kamil@o2.pl>
2018-07-09 17:08:06 +0200

    add_files

--- client/App.js
+++ client/App.js
@@ -53,4 +53,29 @@ renderUserForm() {
     return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
 }
 
+componentDidMount() {
+  socket.on('message', message => this.messageReceive(message));
+  socket.on('update', ({users}) => this.chatUpdate(users));
+}
+
+messageReceive(message) {
+  const messages = [message, ...this.state.messages];
+  this.setState({messages});
+}
+
+chatUpdate(users) {
+  this.setState({users});
+}
+
+handleMessageSubmit(message) {
+  const messages = [message, ...this.state.messages];
+  this.setState({messages});
+  socket.emit('message', message);
+}
+
+handleUserSubmit(name) {
+  this.setState({name});
+  socket.emit('join', name);
+}
+
 export default App;
